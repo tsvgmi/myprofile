24270d09301a2bae9dd0f2978b146414b4047c90
Thien Vuong <thien.vuong@etrade.com>
2017-08-03 00:53:16 -0700

    Refactor and fix like_user

--- bin/mtool/hacauto.rb
+++ bin/mtool/hacauto.rb
@@ -50,7 +50,7 @@ class SDriver
 end
 
 class SPage
-  attr_reader :sdriver
+  attr_reader :sdriver, :page
 
   def initialize(sdriver)
     @sdriver = sdriver
@@ -61,23 +61,26 @@ class SPage
     @page = Nokogiri::HTML(@sdriver.page_source)
   end
 
-  def click_links(lselector, rselector, count=1)
-    links = []
-    @page.css(lselector).each do |asong|
-      link = asong['href']
-      @sdriver.goto(link)
+  def find_and_click_links(lselector, rselector)
+    links = @page.css(lselector).map {|asong| asong['href']}
+    click_links(link, rselector)
+  end
+  
+  def click_links(links, rselector)
+    links.each do |link|
+      goto(link)
       sleep(2)
-      1.upto(count) do
-        @sdriver.click_and_wait(rselector)
-      end
+      @sdriver.click_and_wait(rselector)
       sleep(2)
       links << link
     end
     links
   end
 
+
   def goto(link)
     @sdriver.goto(link)
+    sleep(2)
     refresh
   end
 
@@ -120,48 +123,50 @@ class HACAuto
 
     def rate_today
       _connect_site do |spage|
-        spage.click_links('a.hot-today-item-song',
+        spage.find_and_click_links('a.hot-today-item-song',
                           '#contribute-rating-control')
       end
     end
 
     def rate_week
       _connect_site do |spage|
-        spage.click_links('div#weekly-monthly-list a.song-title',
+        spage.find_and_click_links('div#weekly-monthly-list a.song-title',
                           '#contribute-rating-control')
       end
     end
 
     def rate_user(user, level)
       _each_page("/profile/posted/#{user}") do |spage|
-        spage.click_links('div.song-list a.song-title',
+        spage.find_and_click_links('div.song-list a.song-title',
                           "#contribute-rating-control li:nth-child(#{level})")
       end
     end
 
-    def rate_rhythm(path)
+    def rate_rhythm(path, level=3)
       _each_page("/rhythm/v/#{path}") do |spage|
-        spage.click_links('div.song-list a.song-title',
+        spage.find_and_click_links('div.song-list a.song-title',
                           "#contribute-rating-control li:nth-child(#{level})")
       end
     end
 
-    def rate_genre(path)
+    def rate_genre(path, level=3)
       _each_page("/genre/v/#{path}") do |spage|
-        spage.click_links('div.song-list a.song-title',
+        spage.find_and_click_links('div.song-list a.song-title',
                           "#contribute-rating-control li:nth-child(#{level})")
       end
     end
 
     def like_user(user, nlike=3)
-      # Like is toggle.  2n-1 to get n likes
-      nlike = (nlike.to_i * 2)-1
       _each_page("/profile/posted/#{user}") do |spage|
-        spage.click_links('div.song-list a.song-title',
-                          "#song-favorite-star-btn", nlike)
+        nlinks = []
+        spage.page.css(".song-item").each do |sitem|
+          iclasses = sitem.css('.song-like')[0].attr('class').split
+          next if iclasses.include?('starred')
+          nlinks << sitem.css('.song-title')[0]['href']
+        end
+        spage.click_links(nlinks, "#song-favorite-star-btn")
       end
     end
-
   end
 end
 
